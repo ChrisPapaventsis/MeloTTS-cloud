69c25a5aace37c5f63105b98ac1efca02ed36046
ChrisPapaventsis <ChrisAlexPapav2006@gmail.com>
2025-06-02 10:51:53 +0300

    Reduced dependencies to improve cold start time.

--- download_utils.py
+++ download_utils.py
@@ -8,20 +8,14 @@ DOWNLOAD_CKPT_URLS = {
     'EN': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/EN/checkpoint.pth',
     'EN_V2': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/EN_V2/checkpoint.pth',
     'FR': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/FR/checkpoint.pth',
-    'JP': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/JP/checkpoint.pth',
     'ES': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/ES/checkpoint.pth',
-    'ZH': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/ZH/checkpoint.pth',
-    'KR': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/KR/checkpoint.pth',
 }
 
 DOWNLOAD_CONFIG_URLS = {
     'EN': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/EN/config.json',
     'EN_V2': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/EN_V2/config.json',
     'FR': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/FR/config.json',
-    'JP': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/JP/config.json',
     'ES': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/ES/config.json',
-    'ZH': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/ZH/config.json',
-    'KR': 'https://myshell-public-repo-host.s3.amazonaws.com/openvoice/basespeakers/KR/config.json',
 }
 
 PRETRAINED_MODELS = {
@@ -35,10 +29,7 @@ LANG_TO_HF_REPO_ID = {
     'EN_V2': 'myshell-ai/MeloTTS-English-v2',
     'EN_NEWEST': 'myshell-ai/MeloTTS-English-v3',
     'FR': 'myshell-ai/MeloTTS-French',
-    'JP': 'myshell-ai/MeloTTS-Japanese',
     'ES': 'myshell-ai/MeloTTS-Spanish',
-    'ZH': 'myshell-ai/MeloTTS-Chinese',
-    'KR': 'myshell-ai/MeloTTS-Korean',
 }
 
 def load_or_download_config(locale, use_hf=True, config_path=None):
--- init_downloads.py
+++ init_downloads.py
@@ -8,7 +8,4 @@ if __name__ == '__main__':
         'EN': TTS(language='EN', device=device),
         'ES': TTS(language='ES', device=device),
         'FR': TTS(language='FR', device=device),
-        'ZH': TTS(language='ZH', device=device),
-        'JP': TTS(language='JP', device=device),
-        'KR': TTS(language='KR', device=device),
     }
